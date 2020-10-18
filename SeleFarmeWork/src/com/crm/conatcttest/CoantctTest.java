1e23058876e2f4633385339861791ee2805f5a81
Deepak <Deepak@Ty-Deepak-Tr.lan>
2020-10-18 11:53:39 +0530

    my first commit to local repo

---
+++ SeleFarmeWork/src/com/crm/conatcttest/CoantctTest.java
@@ -0,0 +1,12 @@
+package com.crm.conatcttest;
+
+import org.testng.annotations.Test;
+
+public class CoantctTest {
+	
+
+	 @Test
+	 public void createConatctTest() {
+		 System.out.println("step 1");
+	 }
+}
---
+++ SeleFarmeWork/src/com/crm/conatcttest/Lib.java
@@ -0,0 +1,9 @@
+package com.crm.conatcttest;
+
+public class Lib {
+	
+	public void mtd1() {
+		System.out.println("step-1 ");
+	}
+
+}
