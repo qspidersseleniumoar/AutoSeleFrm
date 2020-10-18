a16e753a55e48d329b2db1860e71d51fc8983e1d
LohithReddy8 <67118468+LohithReddy8@users.noreply.github.com>
2020-10-18 13:06:59 +0530

    Update Lib.java

--- SeleFarmeWork/src/com/crm/conatcttest/Lib.java
+++ SeleFarmeWork/src/com/crm/conatcttest/Lib.java
@@ -6,6 +6,9 @@ public class Lib {
 		System.out.println("step-1 ");
 		System.out.println("step-2 ");
 	}
+	/**
+	   used for ---
+	**/
 		public void mtd2() {
 		System.out.println("step-1 ");
 		System.out.println("step-2 ");
